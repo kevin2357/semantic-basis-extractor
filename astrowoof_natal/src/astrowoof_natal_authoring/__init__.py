c3ac7a818f1af1eed151a3efb7d926243fbfbd67
Kevin Tankersley <kevin2357@gmail.com>
2026-08-13 15:20:02 -0600

    Set 0.3.0 release version

--- astrowoof_natal/src/astrowoof_natal_authoring/__init__.py
+++ astrowoof_natal/src/astrowoof_natal_authoring/__init__.py
@@ -5,7 +5,6 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version("astrowoof-natal-authoring")
 except PackageNotFoundError:
-    __version__ = "0.1.0.dev0"
+    __version__ = "0.3.0.dev0"
 
 __all__ = ["__version__"]
-
